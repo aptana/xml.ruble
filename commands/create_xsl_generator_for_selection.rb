d6862230d3002774d2911983fd4967b73845f1df
sgtcoolguy <cwilliams@aptana.com>
2010-10-08 14:54:34 -0400

    move requires used by command blocks into the blocks themselves to speed up loading of bundles

--- commands/create_xsl_generator_for_selection.rb
+++ commands/create_xsl_generator_for_selection.rb
@@ -1,24 +1,14 @@
 require 'ruble'
-require 'rexml/document'
-
-def serialize(source, dest)
-  if source.is_a? REXML::Text
-    dest.add_text(source.dup)
-    return dest
-  end
-
-  d2 = dest.add_element('xsl:element', 'name' => source.expanded_name)
-  source.attributes.each { |(k,v)| d2.add_element('xsl:attribute', 'name' => k).text = v }
-  source.children.each { |node| serialize(node, d2)}
-  dest
-end
 
 command 'Create XSL Generator for Selection' do |cmd|
   cmd.scope = 'text.xml'
   cmd.output = :replace_selection
   cmd.input = :selection, :document
   cmd.invoke do
-    source = REXML::Document.new(STDIN.read)
+    require 'rexml/document'
+    require 'serialize_xml'
+    
+    source = REXML::Document.new($stdin.read)
     dest   = REXML::Document.new
     
     print serialize(source.elements[1], dest).to_s
