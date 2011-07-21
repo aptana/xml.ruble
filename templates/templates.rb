146f33def49f925e7eea503ef558dbc456316117
Allen Yeung <ayeung@aptana.com>
2011-07-21 14:38:16 -0700

    Revert "Fix APSTUD-2134:Add .location property to file templates"
    This reverts commit d5a10034ef5655bf18d732ebffc121ba42e0accf.

--- templates/templates.rb
+++ templates/templates.rb
@@ -2,10 +2,18 @@ require 'ruble'
 
 template "XML Template" do |t|
   t.filetype = "*.xml"
-  t.location = "templates/template.xml"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.xml")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
 
 template "XSLT Template" do |t|
   t.filetype = "*.xsl"
-  t.location = "templates/template.xsl"
+  t.invoke do |context|
+    ENV['TM_DATE'] = Time.now.strftime("%Y-%m-%d")
+    raw_contents = IO.read("#{ENV['TM_BUNDLE_SUPPORT']}/../templates/template.xsl")
+    raw_contents.gsub(/\$\{([^}]*)\}/) {|match| ENV[match[2..-2]] }
+ end
 end
